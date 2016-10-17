8ae65a1e5d076f7427892b367e61cea26a3ccfa8
Georg Fischer <snorpey@gmail.com>
2016-10-17 11:41:13 +0200

    send spacebrew message when allocation is selected

--- js/raw-material-timeline.js
+++ js/raw-material-timeline.js
@@ -257,6 +257,17 @@ var rawTimeline = {
 		}
 
 		function showAllocation ( allocation, nextAllocation ) {
+			var startOfDayInSecs = parseInt( moment( allocation.start ).hour( 0 ).minute( 0 ).second( 0 ).format( 'X' ), 10 );
+			var endOfDayInSecs = parseInt( moment( allocation.start ).hour( 23 ).minute( 59 ).second( 59 ).format( 'X' ), 10 );
+			var dayDurationInSecs = endOfDayInSecs - startOfDayInSecs;
+			var allocationStartInSecs = parseInt( allocation.start.format( 'X' ), 10 );
+
+			window.sendSpacebrewMessage( 'allocationSelected', {
+				start: allocation.start.format(),
+				end: allocation.end.format(),
+				positionInDay: ( allocationStartInSecs  - startOfDayInSecs ) / dayDurationInSecs
+			} );
+
 			var infoEl = $( '#app-tab2 #first-row-rohstoffe > .ui-tabs-panel[aria-hidden="false"]' );
 			var wrapperEl = $( '#rohstoffe-recipe', infoEl );
 
@@ -264,9 +275,6 @@ var rawTimeline = {
 			var recipe = getAllocationContent( allocation, 'recipe' );
 
 			wrapperEl.attr( 'data-recipe', allocationContent.toLowerCase() );
-
-			// IF ALLOCATION == SETUP: => NEXT RECIPE!
-			console.log( );
 			
 			// UPDATE RECIPE EL
 			if ( recipe ) {
